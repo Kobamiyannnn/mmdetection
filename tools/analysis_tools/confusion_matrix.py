ff026b9447307ff6da29d28ec27b87cf382bab51
Kobamiyannnn <kobaprfm23@gmail.com>
2023-12-05 00:23:52 +0900

    Customized confusion_matrix.py

--- tools/analysis_tools/confusion_matrix.py
+++ tools/analysis_tools/confusion_matrix.py
@@ -2,6 +2,7 @@ import argparse
 import os
 
 import matplotlib.pyplot as plt
+import matplotlib.patheffects as patheffects
 import numpy as np
 from matplotlib.ticker import MultipleLocator
 from mmcv.ops import nms
@@ -154,6 +155,7 @@ def plot_confusion_matrix(confusion_matrix,
                           labels,
                           save_dir=None,
                           show=True,
+                          normalize=True,  # 追加
                           title='Normalized Confusion Matrix',
                           color_theme='plasma'):
     """Draw confusion matrix with matplotlib.
@@ -164,17 +166,26 @@ def plot_confusion_matrix(confusion_matrix,
         save_dir (str|optional): If set, save the confusion matrix plot to the
             given path. Default: None.
         show (bool): Whether to show the plot. Default: True.
+        normalize (bool): category_idの総数に対する要素数の割合を計算する。 Default: True
         title (str): Title of the plot. Default: `Normalized Confusion Matrix`.
         color_theme (str): Theme of the matrix color map. Default: `plasma`.
     """
-    # normalize the confusion matrix
-    per_label_sums = confusion_matrix.sum(axis=1)[:, np.newaxis]
-    confusion_matrix = \
-        confusion_matrix.astype(np.float32) / per_label_sums * 100
+    if normalize:
+        # normalize the confusion matrix
+        per_label_sums = confusion_matrix.sum(axis=1)[:, np.newaxis]
+        confusion_matrix = \
+            confusion_matrix.astype(np.float32) / per_label_sums * 100
+    else:
+        title = 'Confusion Matrix'
+        confusion_matrix.astype(int)  # category_idの数だから整数であることは保証されている
 
     num_classes = len(labels)
+    fig, ax = plt.subplots()
+    '''
+    表示が崩れる（収まりきらない）ため、上のように変更
     fig, ax = plt.subplots(
         figsize=(0.5 * num_classes, 0.5 * num_classes * 0.8), dpi=180)
+    '''
     cmap = plt.get_cmap(color_theme)
     im = ax.imshow(confusion_matrix, cmap=cmap)
     plt.colorbar(mappable=im, ax=ax)
@@ -209,20 +220,47 @@ def plot_confusion_matrix(confusion_matrix,
     plt.setp(
         ax.get_xticklabels(), rotation=45, ha='left', rotation_mode='anchor')
 
-    # draw confution matrix value
-    for i in range(num_classes):
-        for j in range(num_classes):
-            ax.text(
-                j,
-                i,
-                '{}%'.format(
-                    int(confusion_matrix[
-                        i,
-                        j]) if not np.isnan(confusion_matrix[i, j]) else -1),
-                ha='center',
-                va='center',
-                color='w',
-                size=7)
+    # TODO: とりあえずの実装
+    if normalize:
+        # draw confution matrix value
+        for i in range(num_classes):
+            for j in range(num_classes):
+                ax.text(
+                    j,
+                    i,
+                    '{}%'.format(
+                        int(confusion_matrix[
+                            i,
+                            j]) if not np.isnan(confusion_matrix[i, j]) else -1),
+                    horizontalalignment='center',
+                    verticalalignment='center',
+                    color='w',  # default: w
+                    fontsize=10,  # default: 7
+                    fontweight='normal',  # default: normal
+                    path_effects=[patheffects.withStroke(linewidth=2,
+                                                         foreground='black',
+                                                         capstyle="round")]  # By myself
+                )
+    else:
+        # draw confution matrix value
+        for i in range(num_classes):
+            for j in range(num_classes):
+                ax.text(
+                    j,
+                    i,
+                    '{}'.format(
+                        int(confusion_matrix[
+                            i,
+                            j]) if not np.isnan(confusion_matrix[i, j]) else -1),
+                    horizontalalignment='center',
+                    verticalalignment='center',
+                    color='w',  # default: w
+                    fontsize=10,  # default: 7
+                    fontweight='normal',  # default: normal
+                    path_effects=[patheffects.withStroke(linewidth=2,
+                                                         foreground='black',
+                                                         capstyle="round")]  # By myself
+                )
 
     ax.set_ylim(len(confusion_matrix) - 0.5, -0.5)  # matplotlib>3.1.1
 
@@ -266,6 +304,7 @@ def main():
         dataset.metainfo['classes'] + ('background', ),
         save_dir=args.save_dir,
         show=args.show,
+        normalize=False,
         color_theme=args.color_theme)
 
 
